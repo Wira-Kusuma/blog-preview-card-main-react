dfd8ff0e641d17e9974ed61c9bebcf93bead476c
Wira Kusuma Phandawa <wirakusuma5566@gmail.com>
2025-08-06 00:04:07 +0700

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import "./images/favicon-32x32.png"
+import "../src/images/favicon-32x32.png"
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
