89696006cff820615d6a5358c05824dd1d484823
Wira Kusuma Phandawa <wirakusuma5566@gmail.com>
2025-08-05 21:08:40 +0700

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,28 @@
+// import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const item = {
+    userImg:"./assets/images/image-avatar.webp",
+    illustrationImg:"./assets/images/illustration-article.svg",
+    title:"HTML & CSS foundations"
+  }
+  return(
+    <header>
+      <img src={item.illustrationImg} alt="" />
+      
+      <h1>{item.title}</h1>
+      <Status />
+    </header>
+  );  
+}
+
+function Status() {
+  return(
+    <div>
+      <p>learning</p>
+    </div>
+  );
+}
+
+export default App
