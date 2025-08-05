86fb62e2caa87f58797500691728f1f526f82adf
Wira Kusuma Phandawa <wirakusuma5566@gmail.com>
2025-08-05 22:07:19 +0700

    publish

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,25 @@
 // import { useState } from 'react'
 import './App.css'
 
-function App() {
-  const item = {
-    userImg:"./assets/images/image-avatar.webp",
+
+const item = {
     illustrationImg:"./assets/images/illustration-article.svg",
-    title:"HTML & CSS foundations"
-  }
+    title:"HTML & CSS foundations",
+    published:"21 Dec 2023",
+    description:"These languages are backbone of every website, defining structure, content, and presentation",
+    username:"Greg Hooper",
+    userImg:"./assets/images/image-avatar.webp"
+}
+
+function App() {
   return(
     <header>
-      <img src={item.illustrationImg} alt="" />
-      
-      <h1>{item.title}</h1>
+      <img src={item.illustrationImg} alt={item.title} />
       <Status />
+      <Date />
+      <h1>{item.title}</h1>
+      <Description/>
+      <User/>
     </header>
   );  
 }
@@ -23,4 +30,24 @@ function Status() {
   );
 }
 
+function Date() {
+  return(
+    <p className='date'>Published {item.published}</p>
+  );
+}
+
+function Description() {
+  return(
+    <p className='desc'>{item.description}</p>
+  );
+}
+
+function User() {
+  return(
+    <div className="user">
+      <img src={item.userImg} alt={item.username} />
+      <p className='username'>{item.username}</p>
+    </div>
+  )
+}
 export default App
